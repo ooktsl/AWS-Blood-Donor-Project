465e20e1c0a61aacd38300026d2fd7a4045b4813
ooktsl <omerkutsal@gmail.com>
2024-12-01 22:02:05 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 
 # Update the DATABASE_URL for MySQL on AWS RDS
 # Replace <username>, <password>, <hostname>, <dbname> with your actual MySQL credentials and database details
-DATABASE_URL = "mysql+pymysql://admin:haydegidelum@kanbagisdb.clwamwmci9vk.eu-central-1.rds.amazonaws.com:63306/kanbagisdb"
+DATABASE_URL = ""
 
 # Create engine for connecting to MySQL
 engine = create_engine(DATABASE_URL, pool_size=20, max_overflow=10, pool_timeout=30, pool_recycle=3600)
@@ -20,10 +20,3 @@ Base = _declarative.declarative_base()
 
 
 
-
-"""# SQLITE
-DATABASE_URL = "sqlite:///./database.db"
-engine = _sql.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = _declarative.declarative_base()
-"""
